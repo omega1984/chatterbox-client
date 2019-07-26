56aa6535e96f484bc7e115d44d391e3a84bc2c7a
omega1984 <omegareborn2019@gmail.com>
2019-07-26 15:08:49 -0700

    passed a few tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -16,33 +16,11 @@ var App = {
     App.fetch(App.stopSpinner);
 
   },
+  // addMessage: function(){
+  //   var message = $('<p class=message></p>');
 
-  send: function() {
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
-      type: 'POST',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent');
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
-      }
-    });
-  }, 
-
-  clearMessages: function(){
-    $('#chat').empty();
-  }, 
-
-  addMessage: function(){
-    var message = $('<p class=message></p>');
-
-    $('#chat').append(message);
-  },
+  //   $('#chat').append(message);
+  // },
 
   fetch: function(callback = ()=>{}) {
     Parse.readAll((data) => {
